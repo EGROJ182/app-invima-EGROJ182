00718036021e7cd975af899a107bf56dbf8d7f04
JORGE EDILSON VEGA ACOSTA <jorgevega@gestarinnovacion.com>
2024-02-12 20:35:55 -0500

    App Terminada Actualización Final Jorge Vega

---
+++ Config/config.js
@@ -0,0 +1,11 @@
+// Url API
+const CURRENT_MEDICATIONS = 'https://www.datos.gov.co/resource/i7cb-raxc';//Vigentes
+const MEDICATIONS_IN_THE_RENEWAL_PROCESS = 'https://www.datos.gov.co/resource/vgr4-gemg';//En tramite de Renovación
+const EXPIRED_MEDICATIONS = 'https://www.datos.gov.co/resource/vwwf-4ftk';//Vencidos
+const MEDICATIONS_OTHER_STATES = 'https://www.datos.gov.co/resource/spzp-dfuc';//otros estados
+const PMVMXPC = "https://www.datos.gov.co/resource/nauz-qkjw";// Precio máximo de venta de los medicamentos por presentación comercial
+
+// const CURRENT_MEDICATIONS = 'https://www.datos.gov.co/resource/i7cb-raxc.json';//Vigentes
+// const MEDICATIONS_IN_THE_RENEWAL_PROCESS = 'https://www.datos.gov.co/resource/vgr4-gemg.json';//En tramite de Renovación
+// const EXPIRED_MEDICATIONS = 'https://www.datos.gov.co/resource/vwwf-4ftk.json';//Vencidos
+// const MEDICATIONS_OTHER_STATES = 'https://www.datos.gov.co/resource/spzp-dfuc.json';//otros estados
